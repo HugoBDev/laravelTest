ed4b4c62090f9216d7c36fd44d6dad460a7ae792
HugoBDev <hugobrd.pro@gmail.com>
2024-08-26 14:06:59 +0200

    setting up project, testing routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/accueil', [HelloController::class, "index"]);
