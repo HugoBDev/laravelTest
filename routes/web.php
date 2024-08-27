655f52dabaf04f45615f83464cc52f774159f3dc
HugoBDev <hugobrd.pro@gmail.com>
2024-08-27 16:22:27 +0200

    discovered Models, Migrations, and added a DB to work with it

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
-
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +10,27 @@ Route::get('/', function () {
 
 
 Route::get('/accueil', [HelloController::class, "index"]);
+
+
+
+
+Route::prefix('/blog')->group(function () {
+    Route::get('/', function () {
+
+        $post = new Post();
+        $post->title = 'mon premier article'; 
+        $post->slug = 'mon premier-article';
+        $post->content = 'Mon contenu';
+        $post->save();
+
+     return $post ;
+
+
+
+     
+        return view('welcome');
+    });
+    Route::get('/test', function () {
+        return 'ceci est le test';
+    })->name('blog.show');
+});
